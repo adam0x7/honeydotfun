286ea8786b28a6c67cc3a1a54245c7b5496b6d38
adam0x7 <18746720+adam0x7@users.noreply.github.com>
2024-07-01 23:46:20 +0200

    adding remappings and deployment logic

--- contracts/src/HoneyTokenFactory.sol
+++ contracts/src/HoneyTokenFactory.sol
@@ -11,35 +11,51 @@ Go through first steps: purchasing memecoin on a bonding curve. what does that e
 */
 pragma solidity ^0.8.0;
 
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Owned} from "solmate/auth/Owned.sol";
 
-contract HoneyTokenFactory {
+contract HoneyTokenFactory is Owned {
+
+    event TokenCreated(uint256 indexed id, string name, string memeUrl, address token, address deployer);
 
     address owner;
 
-    address public constant _CREATE2_FACTORY = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+    address public constant beraCreate2Factory = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
 
     address v2Pool;
 
-    address router;
-
     struct Token {
         uint256 id;
+        uint256 decimals;
         string name;
         string memeUrl;
         address token;
         address deployer;
     }
 
-    constructor(address _owner) {
-        owner = _owner;
-    }
+    mapping(uint256 => Token) public tokens;
+    uint256 tokenCount;
 
-    function createMemecoin(bytes calldata data) public returns(bytes) {
+    constructor() Owned(msg.sender) {}
+
+    function createMemecoin(bytes calldata data) external returns(token) {
         Token memory newToken = abi.decode(data, (Token));
 
         bytes32 salt = keccak256(abi.encodePacked(data, msg.sender));
+        bytes memory initCode = abi.encodePacked(
+        type(ERC20).creationCode,
+        abi.encode(newToken.name, newToken.symbol, newToken.decimals)
+        );
+        address token = beraCreate2Factory.deployWithCreate2(salt, initCode);
 
+        newToken.token = token;
+        newToken.deployer = msg.sender;
+        newToken.id = ++tokenCount;
+        tokens[newToken.id] = newToken;
 
+        emit TokenCreated(newToken.id, newToken.name, newToken.memeUrl, token, msg.sender);
+
+        return token;
     }
 
 
