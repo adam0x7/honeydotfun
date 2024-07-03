e5a850abc202a41a189c4f5097802aba7483d31c
adam0x7 <18746720+adam0x7@users.noreply.github.com>
2024-07-03 22:27:41 +0200

    Update MiniUniswapV2Pool.sol

--- contracts/src/MiniUniswapV2Pool.sol
+++ contracts/src/MiniUniswapV2Pool.sol
@@ -3,63 +3,75 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+interface IWBERA is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint256) external;
+}
+
 contract MiniUniswapV2Pool is ERC20 {
+    IWBERA public immutable wbera;
     IERC20 public immutable token;
-    uint public reserve0; // BERA reserve
+    uint public reserve0; // WBERA reserve
     uint public reserve1; // token reserve
 
-    constructor(address _token) ERC20("MiniUniLP", "MLPT") {
+    constructor(address _token, address _wbera) ERC20("MiniUniLP", "MLPT") {
         token = IERC20(_token);
+        wbera = IWBERA(_wbera);
     }
 
-    function addLiquidity() external payable {
-        uint token1Amount = token.balanceOf(address(this)) - reserve1;
-        uint liquidity;
-        uint balance0 = address(this).balance;
-        uint amount0 = balance0 - reserve0;
+    function addLiquidity() external returns (uint256 liquidity) {
+        uint256 balance0 = wbera.balanceOf(address(this));
+        uint256 balance1 = token.balanceOf(address(this));
+        uint256 amount0 = balance0 - reserve0;
+        uint256 amount1 = balance1 - reserve1;
 
         if (totalSupply() == 0) {
-            liquidity = Math.sqrt(amount0 * token1Amount);
+            liquidity = Math.sqrt(amount0 * amount1);
         } else {
             liquidity = Math.min(
                 (amount0 * totalSupply()) / reserve0,
-                (token1Amount * totalSupply()) / reserve1
+                (amount1 * totalSupply()) / reserve1
             );
         }
 
         require(liquidity > 0, "Insufficient liquidity minted");
         _mint(msg.sender, liquidity);
 
-        _update(balance0, token.balanceOf(address(this)));
+        _update(balance0, balance1);
+        return liquidity;
     }
 
-    function removeLiquidity(uint liquidity) external {
+    function removeLiquidity(uint liquidity) external returns (uint256 amount0, uint256 amount1) {
         require(balanceOf(msg.sender) >= liquidity, "Insufficient liquidity");
-        uint amount0 = (liquidity * reserve0) / totalSupply();
-        uint amount1 = (liquidity * reserve1) / totalSupply();
+        amount0 = (liquidity * reserve0) / totalSupply();
+        amount1 = (liquidity * reserve1) / totalSupply();
 
         _burn(msg.sender, liquidity);
         _update(reserve0 - amount0, reserve1 - amount1);
 
-        payable(msg.sender).transfer(amount0);
+        wbera.transfer(msg.sender, amount0);
         token.transfer(msg.sender, amount1);
+        return (amount0, amount1);
     }
 
-    function swapBERAForToken(uint minTokens) external payable {
-        uint tokensBought = getAmountOut(msg.value, reserve0, reserve1);
+    function swapWBERAForToken(uint256 minTokens) external returns (uint256 tokensBought) {
+        uint256 wberaIn = wbera.balanceOf(address(this)) - reserve0;
+        tokensBought = getAmountOut(wberaIn, reserve0, reserve1);
         require(tokensBought >= minTokens, "Insufficient output amount");
 
         token.transfer(msg.sender, tokensBought);
-        _update(address(this).balance, token.balanceOf(address(this)));
+        _update(wbera.balanceOf(address(this)), token.balanceOf(address(this)));
+        return tokensBought;
     }
 
-    function swapTokenForBERA(uint tokenAmount, uint minBERA) external {
-        uint beraBought = getAmountOut(tokenAmount, reserve1, reserve0);
-        require(beraBought >= minBERA, "Insufficient output amount");
+    function swapTokenForWBERA(uint256 tokenAmount, uint256 minWBERA) external returns (uint256 wberaBought) {
+        wberaBought = getAmountOut(tokenAmount, reserve1, reserve0);
+        require(wberaBought >= minWBERA, "Insufficient output amount");
 
         token.transferFrom(msg.sender, address(this), tokenAmount);
-        payable(msg.sender).transfer(beraBought);
-        _update(address(this).balance, token.balanceOf(address(this)));
+        wbera.transfer(msg.sender, wberaBought);
+        _update(wbera.balanceOf(address(this)), token.balanceOf(address(this)));
+        return wberaBought;
     }
 
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) public pure returns (uint amountOut) {
@@ -73,8 +85,6 @@ contract MiniUniswapV2Pool is ERC20 {
 
     function _update(uint _reserve0, uint _reserve1) private {
         reserve0 = _reserve0;
-        reserve1 = _reset1;
+        reserve1 = _reserve1;
     }
-
-    receive() external payable {}
 }
